c00cd4ac0a32c522a1022e181f3abe8e4ad5cfe0
dreyjo <59994482+dreyjo@users.noreply.github.com>
2023-11-28 16:52:01 -0500

    added new acq id pattern, deleted old comments

--- Mac/kryofluxmove.sh
+++ Mac/kryofluxmove.sh
@@ -10,26 +10,13 @@ echo -e  "${BLUE}Please enter your collection number to initiate the move.${NC}"
 
 read collnum
 
-#find . -name "$collnum-*" > tempfile | md5deep -f tempfile > /Volumes/Staging/ingest/kryofluxOutput/tempchecksums
-
-#rm tempfile
-
-#md5deep -r /Volumes/Staging/ingest/diskImages/$collnum > /Volumes/Staging/ingest/kryofluxOutput/tempchecksums2
-
-#echo -e "${GREEN}1. Checksums created.${NC}"
-
-#cat /Volumes/Staging/ingest/kryofluxOutput/tempchecksums2 >> /Volumes/Staging/ingest/kryofluxOutput/tempchecksums 
-
-#find . -name "$collnum-*" | cp 
-
 for f in `find . -name "$collnum-*.tar"`; do 
 	directory=`echo "$f" | cut -d \. -f 2 | cut -d \/ -f 2`
 	mv $f /Volumes/Staging/KFStreamArchive
 
 done
 
-#is collnum just whatever was entered after the first question? 
-for f in `find . -name "$collnum_*_*.tar"`; do 
+for f in `find . -name "$collnum_*.tar"`; do 
 	directory=`echo "$f" | cut -d \. -f 2 | cut -d \/ -f 2`
 	mv $f /Volumes/Staging/KFStreamArchive
 
@@ -41,7 +28,7 @@ for f in `find . -name "$collnum-*.001"`; do
 
 done
 
-for f in `find . -name "$collnum_*_*.001"`; do 
+for f in `find . -name "collnum_*.001"`; do 
 	directory=`echo "$f" | cut -d \. -f 2 | cut -d \/ -f 2`
 	mv $f /Volumes/Staging/ingest/diskImages/$collnum/$directory/objects/
 
@@ -53,7 +40,7 @@ for f in `find . -name "$collnum-*.img"`; do
 
 done
 
-for f in `find . -name "$collnum_*_*.img"`; do
+for f in `find . -name "collnum_*.img"`; do
 	directory=`echo "$f" | cut -d \. -f 2 | cut -d \/ -f 2`
 	mv $f /Volumes/DigArchDiskStation/Staging/ingest/diskImages/$collnum/$directory/objects/
 
@@ -65,7 +52,7 @@ for f in `find . -name "$collnum-*.log"`; do
 
 done
 
-for f in `find . -name "$collnum_*_*.log"`; do 
+for f in `find . -name "$collnum_*.log"`; do 
 	directory=`echo "$f" | cut -d \. -f 2 | cut -d \/ -f 2`
 	mv $f /Volumes/Staging/ingest/diskImages/$collnum/$directory/metadata/
 
@@ -73,19 +60,4 @@ done
 
 echo -e "${GREEN}Disk image has been transferred to diskIMages and steam files transferred to KFSteamArchive.${NC}"
 
-#cd /Volumes/Staging/ingest/diskImages/$collnum
-
-#md5deep -x /Volumes/Staging/ingest/kryofluxOutput/tempchecksums *
-
-#if [[ $(ls -A) ]]; then
-	
-
-#	echo -e "${GREEN}3. Files have been validated.${NC}" 
-#else 
-	#echo -e "${RED}Validation failed.${NC}"
-#fi
-
-#rm /Volumes/Staging/ingest/kryofluxOutput/tempchecksums 
-
-#rm /Volumes/Staging/ingest/kryofluxOutput/tempchecksums2
 
